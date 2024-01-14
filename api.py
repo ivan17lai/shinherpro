1ef656dc81623d689efd2250ab081329fe27fce2
yihuan <ivan17.lai@gmail.com>
2024-01-14 21:59:42 +0800

    final exam preparation

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
+from flask import render_template
 import tyaiShinher
 import tyaiPracticeExam
 import time
 import json
 app = Flask(__name__)
+
 
 @app.route('/examScore')
 def example():
@@ -51,7 +53,32 @@ def get_work():
 
     return json.dumps(response, ensure_ascii=False, indent=2)
 
+@app.route('/web')
+def get_web():
+
+    return render_template('main.html')
+
+@app.route('/login')
+def login():
+    
+    try:
+        start_time = time.time()
+
+        arg1 = request.args.get('schoolNumber')
+        arg2 = request.args.get('studentID')
+
+        response = tyaiShinher.login(arg1, arg2)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+
+        print(f"Request processed in {elapsed_time} seconds")
+
+    except:
+        response = 'fail'
+
+    return response
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+
+    app.run(host='0.0.0.0', port=5000)

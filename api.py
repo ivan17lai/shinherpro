be9f399514521cea87b71899b0856eb36e5ef0cc
yihuan <ivan17.lai@gmail.com>
2024-01-15 21:23:53 +0800

    Add cheak mail

--- api.py
+++ api.py
@@ -3,6 +3,10 @@ from flask import render_template
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 import gspread
 from google.oauth2 import service_account
 from google.auth.transport.requests import Request
@@ -19,6 +23,48 @@ CORS(app)
 socketio = SocketIO(app)
 
 ngrok_code = "8926"
+
+
+def send_mail(mailadd,name,number):
+
+    file_path = 'gmail_password.txt'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        file_content = file.read()
+
+    gmail_user = 'shinherpro@gmail.com'
+    gmail_password = file_content
+
+    to_email = mailadd
+
+    subject = f'Hi!{name},歡迎使用shinherPro🙂'
+
+    file_path = 'templates\\cheakMail.html'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        html_content = file.read()
+
+    beta_user = ['013333','013314','013322','013325','013315','013304','013328','013335','013320','013308','013326','013321']
+
+    if number in beta_user:
+        file_path = 'templates\\betauser.html'
+        with open(file_path, 'r', encoding='utf-8') as file:
+            beta_html_content = file.read()
+        html_content = html_content.replace('<!--beta User-->',beta_html_content)
+
+    message = MIMEMultipart()
+    message['From'] = gmail_user
+    message['To'] = to_email
+    message['Subject'] = subject
+
+    html_part = MIMEText(html_content, 'html')
+    message.attach(html_part)
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(gmail_user, to_email, message.as_string())
+
+    print('HTML Email sent successfully!')
+
 
 @app.route('/examScore')
 def example():
@@ -71,7 +117,7 @@ def get_web():
 
     return render_template('main.html')
 
-def writeData(args):
+def writeData(args,name,number):
 
     arg1 = args.get('schoolNumber')
     arg2 = args.get('studentID')
@@ -90,15 +136,10 @@ def writeData(args):
 
     b_column_data = worksheet.col_values(2)
 
-    for i in b_column_data:
-        print(i==data_to_append[1])
-        print(i)
-    print("---")
-    print(data_to_append[0])
-
     if data_to_append[0] not in b_column_data:
         worksheet.append_row(data_to_append)
         print("數據已成功添加到試算表。")
+        send_mail(arg3,name,number)
         response = "success"
     else:
         print("B列已經存在相同的宿。")
@@ -107,6 +148,8 @@ def writeData(args):
     print("數據已成功寫入試算表。")
 
     return response
+
+
 
 @app.route('/login')
 def login():
@@ -120,7 +163,7 @@ def login():
         
         response = tyaiShinher.login(arg1, arg2)
 
-        writeData(request.args)
+        writeData(request.args,response,arg1)
 
         end_time = time.time()
         elapsed_time = end_time - start_time
